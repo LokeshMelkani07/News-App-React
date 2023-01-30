9db7980ca18dd79914f4a7de02ce33a2ec6f30da
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2023-01-30 20:08:38 +0530

    day 2 done api called

--- newsapp/src/Stories.jsx
+++ newsapp/src/Stories.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
 
 const Stories = () => {
-  return <div>News Stories</div>;
+  let API = "https://hn.algolia.com/api/v1/search?";
+
+  const fetchApiData = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApiData(API);
+  }, []);
+
+  return (
+    <>
+      <h2>Tech News</h2>
+    </>
+  );
 };
 
 export default Stories;
